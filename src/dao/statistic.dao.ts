783e69d5bfa710102c7055c54ebfa6d8a6ea9f1b
clem180 <clemdeangers@outlook.fr>
2021-12-01 22:24:15 +0100

    Fix for url for docker Fix CMD Dockerfile

--- src/dao/statistic.dao.ts
+++ src/dao/statistic.dao.ts
@@ -5,7 +5,7 @@ const fetch =  require('node-fetch');
 export class StatisticDAO {
 
     async getStatisticByUser(user: string) : Promise<GameModel[]>{
-        const game = await fetch('http://10.44.17.0:7510/game/user/'+user);
+        const game = await fetch('http://micro-service-game:3000/game/user/'+user);
         return await game.json() as GameModel[];
     }
 
